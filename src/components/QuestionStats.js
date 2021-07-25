0a7958e0b66766e35ad0d369dbbbe9a6c19e63de
Kevin Martin <kevinmartin33@gmil.com>
2021-07-25 21:00:41 +0100

    Remove class name collision

--- src/components/QuestionStats.js
+++ src/components/QuestionStats.js
@@ -19,12 +19,12 @@ export default class QuestionStats extends React.Component {
                             <div className="grid-y full-heihgt">
                                 <Timer timer={this.props.timer} />
                                 <div className="cell small-1"></div>
-                                <div className="cell small-3 medium-2 game-text">{this.props.questionText}</div>
+                                <div className="cell small-3 medium-2 question-text">{this.props.questionText}</div>
                                 <div className="cell small-2 medium-1"></div>
-                                <div className="cell small-1 game-text">A. {this.props.answers && this.props.answers['A']}: {this.props.questionMetrics && this.props.questionMetrics['A']} / {total_votes}</div>
-                                <div className="cell small-1 game-text">B. {this.props.answers && this.props.answers['B']}: {this.props.questionMetrics && this.props.questionMetrics['B']} / {total_votes}</div>
-                                <div className="cell small-1 game-text">C. {this.props.answers && this.props.answers['C']}: {this.props.questionMetrics && this.props.questionMetrics['C']} / {total_votes}</div>
-                                <div className="cell small-1 game-text">D. {this.props.answers && this.props.answers['D']}: {this.props.questionMetrics && this.props.questionMetrics['D']} / {total_votes}</div>
+                                <div className="cell small-1 question-text">A. {this.props.answers && this.props.answers['A']}: {this.props.questionMetrics && this.props.questionMetrics['A']} / {total_votes}</div>
+                                <div className="cell small-1 question-text">B. {this.props.answers && this.props.answers['B']}: {this.props.questionMetrics && this.props.questionMetrics['B']} / {total_votes}</div>
+                                <div className="cell small-1 question-text">C. {this.props.answers && this.props.answers['C']}: {this.props.questionMetrics && this.props.questionMetrics['C']} / {total_votes}</div>
+                                <div className="cell small-1 question-text">D. {this.props.answers && this.props.answers['D']}: {this.props.questionMetrics && this.props.questionMetrics['D']} / {total_votes}</div>
                             </div>
                         </div>
                         <div className="cell small-2"></div>
