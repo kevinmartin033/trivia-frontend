6a685c65db131d371f53f3cafa179c48cc28078c
Kevin Martin <kevinmartin33@gmil.com>
2021-07-25 01:08:54 +0100

    Use secure socket

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const API_URL = "https://morning-savannah-15360.herokuapp.com"
-export const SOCKET_URL = "ws://morning-savannah-15360.herokuapp.com"
+export const SOCKET_URL = "wss://morning-savannah-15360.herokuapp.com"
